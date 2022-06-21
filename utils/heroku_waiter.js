117a8d204d70b97f4daa05e5e539c36b5cbe3440
Aaron <jew.wallet@internet.ru>
2022-06-21 17:07:31 +0300

    Second commit

--- utils/heroku_waiter.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios').default;
-
-// Этот скрипт создан для того, чтобы Heroku не завершал процесс работы бота.
-
-setInterval(() => {
-   axios.get('https://jsonplaceholder.typicode.com/todos/1')
-       .then(response => console.log(response.data));
-}, 2000);
